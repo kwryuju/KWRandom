22d832bce5adebbd71dbd5391ff267adad351e9a
kwhome <thisistatsuki@gmail.com>
2016-02-11 17:47:05 +0900

    random numer in C

--- KWRandomC/Source.c
+++ KWRandomC/Source.c
@@ -1,4 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+
+#define RANGE_MAX 100
+
 int main() {
+	double real_val;
+	int integer_val;
+
+	/*シード値を現在時から設定*/
+	srand((unsigned int)time(NULL));
+
+	/*0.0から1.0の範囲の実数で乱数を生成*/
+	real_val = rand() / (double)RAND_MAX;
+
+	/*0からRANGE_MAX(=100)の範囲の整数で乱数を生成*/
+	integer_val = rand() % RANGE_MAX;
 
 	return 0;
 }
