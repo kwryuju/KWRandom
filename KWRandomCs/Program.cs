1fece6afa4aef4826cd40d6eb99ffb5aa383a6de
kwhome <thisistatsuki@gmail.com>
2016-02-13 00:12:32 +0900

    random number in C Sharp

--- KWRandomCs/Program.cs
+++ KWRandomCs/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KWRandomCs
 {
     class Program
     {
+        const int RANGE_MAX = 100;
+
         static void Main(string[] args)
         {
+            Random rnd = new Random(Environment.TickCount);
+            
+            Console.WriteLine("{0}, {1}", rnd.Next(0, RANGE_MAX), rnd.NextDouble());
         }
     }
 }
